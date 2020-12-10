484b13d57bf9e8a123041258a37741aa3f3b7fa0
Khriston <33064635+Khriston@users.noreply.github.com>
2020-12-10 15:42:23 +0200

    Rezolvat bug parcurgere parola
    Parola nu era parcursa continuu in timpul criptarii/decriptarii

--- FileCryptor.py
+++ FileCryptor.py
@@ -1,35 +1,36 @@
 import sys
 import hashlib
+import os
 
 def encrypt(arg,password):
     g = open((arg+".crypted"),"at",encoding="utf-8")  
     m = hashlib.sha1()
-    f = open(arg,"rb")
+    f = open(arg,"r")
     while True:
         data = f.read(4096)
         if len(data)==0: break
-        m.update(data)
+        m.update(data.encode())
     f.close()
     has=m.hexdigest()
-    print(has)
+    #print(has)
     g.write(str(has) + '\n')
     with open(arg,encoding="utf-8") as f:
+        i = 0
         for line in f:   
-            i = 0
             newText = ''
             for char in line:
                 newChar = ord(char) + ord(password[ i % len(password)]) #vezi cerinta pt detalii
                 if newChar > 255: #tratez cazul in care suma >255; trunchiez sirul la primii 8 biti
                     binary = bin(newChar)
-                    binary = binary[:len(binary)] 
+                    binary = "0b" + binary[3:] 
                     newChar = int(binary,2)
-                newText = newText + chr(newChar)
+                #print("Caracter:"+char+" "+chr(newChar)+" "+str(ord(char))+" "+ str(newChar))
+                newText += chr(newChar)
                 i =+ 1 #index pentru caracterul curent
             g.write(newText)
     g.close()
-    f.close()
     print("Done encrypting.")
-    
+
 def decrypt(arg,password):
     if ".crypted" not in arg:
         print("Wrong filetype. Needs to be <.crypted>.")
@@ -38,28 +39,31 @@ def decrypt(arg,password):
     g = open(arg[:-8],"at",encoding="utf-8")
     with open(arg,encoding="utf-8") as f:
         flag = 1
+        i = 0
         for line in f:   
             if flag:
                 original = line
                 flag = 0
             else:
-                i = 0
                 newText = ''
                 for char in line:
-                    newChar = ord(char) - ord(password[ i % len(password)]) #vezi cerinta pt detalii
+                    newChar = ord(char) - ord(password[i % len(password)]) #vezi cerinta pt detalii
                     if newChar < 0: #tratez cazul in care diff<0
-                        newChar = 255 + newChar
-                    newText += str(chr(newChar))
+                        newChar = 255 + newChar 
+                    newText += chr(newChar)
+                    #print(chr(newChar)+" "+str(newChar))#+" "+str(prevChar))
                     i =+ 1 #index pentru caracterul curent
                 m.update(newText.encode('utf-8'))
                 g.write(newText)
     f.close()
     g.close()
+    #print(str(m.hexdigest()).strip()+" "+str(original).strip())
     if str(m.hexdigest()).strip() == str(original).strip():
         print("Done decrypting.")
     else:
         print("Wrong password.")
-        
+        os.remove(arg[:-8])
+
 def command(arg):
     if arg=="encrypt":
         encrypt(sys.argv[2],sys.argv[3])
