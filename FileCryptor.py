2d0149b7897c4c649bb87433978d3595572dd66e
Khriston <33064635+Khriston@users.noreply.github.com>
2020-12-17 15:33:37 +0200

    Final

--- FileCryptor.py
+++ FileCryptor.py
@@ -3,31 +3,32 @@ import hashlib
 import os
 
 def encrypt(arg,password):
-    g = open((arg+".crypted"),"at", encoding="utf-8")  
+    g = open((arg+".crypted"),"at")  
     m = hashlib.sha1()
-    f = open(arg,"r")
+    f = open(arg,"rb")
     while True:
         data = f.read(4096)
         if len(data)==0: break
-        m.update(data.encode())
+        m.update(data)
     f.close()
     has=m.hexdigest()
     #print(has)
-    g.write(str(has) + '\n')
-    with open(arg, encoding="utf-8") as f:
-        i = 0
-        for line in f:   
-            newText = ''
-            for char in line:
-                newChar = ord(char) + ord(password[ i % len(password)]) #vezi cerinta pt detalii
-                if newChar > 255: #tratez cazul in care suma >255; trunchiez sirul la primii 8 biti
-                    binary = bin(newChar)
-                    binary = "0b" + binary[3:] 
-                    newChar = int(binary,2)
-                #print("Caracter:"+char+" "+chr(newChar)+" "+str(ord(char))+" "+ str(newChar))
-                newText += chr(newChar)
-                i =+ 1 #index pentru caracterul curent
-            g.write(newText)
+    g.write(has)
+    g.close()
+    g = open((arg+".crypted"),"ab")
+    with open(arg, "rb") as f:
+        i = 0  
+        limit = len(password)
+        byte = f.read(1)
+        while byte:
+            #print(byte)
+            newChar = ord(byte) + ord(password[ i % limit])
+            if newChar > 255: #tratez cazul in care suma >255; trunchiez sirul la primii 8 biti
+                newChar = newChar & 255
+            #print(str(byte)+" " + str(ord(byte))+" "+ str(newChar))
+            i += 1 #index pentru caracterul curent
+            g.write(newChar.to_bytes(1,'little'))
+            byte = f.read(1)
     g.close()
     print("Done encrypting.")
 
@@ -36,25 +37,27 @@ def decrypt(arg,password):
         print("Wrong filetype. Needs to be <.crypted>.")
         return 0
     m = hashlib.sha1()
-    g = open(arg[:-8],"at",encoding="utf-8")
-    with open(arg,encoding="utf-8") as f:
-        flag = 1
-        i = 0
-        for line in f:   
-            if flag:
-                original = line
-                flag = 0
-            else:
-                newText = ''
-                for char in line:
-                    newChar = ord(char) - ord(password[i % len(password)]) #vezi cerinta pt detalii
-                    if newChar < 0: #tratez cazul in care diff<0
-                        newChar = 255 + newChar 
-                    newText += chr(newChar)
-                    #print(chr(newChar)+" "+str(newChar))#+" "+str(prevChar))
-                    i =+ 1 #index pentru caracterul curent
-                m.update(newText.encode('utf-8'))
-                g.write(newText)
+    g = open(arg[:-8],"ab")
+    f = open(arg,"rb")
+    i=40
+    original = ""
+    while i>0:
+        original += chr(ord(f.read(1)))
+        i = i - 1
+    limit = len(password)
+    i = 0 
+    byte = f.read(1)
+    while byte:
+        newChar = ord(byte) - ord(password[ i % limit])
+        if newChar < 0: #tratez cazul in care diff<0
+            newChar = 256 + newChar 
+        #print(chr(newChar)+" "+str(newChar))#+" "+str(prevChar))
+        i += 1 #index pentru caracterul curent
+        #print(str(byte)+" " + str(ord(byte))+" " + str(newChar)+" "+str(newChar.to_bytes(1,'big')))
+        m.update(newChar.to_bytes(1,'big'))
+        g.write(newChar.to_bytes(1,'big'))
+        #print(newChar.to_bytes(2,'little'))
+        byte = f.read(1)
     f.close()
     g.close()
     #print(str(m.hexdigest()).strip()+" "+str(original).strip())
@@ -62,7 +65,7 @@ def decrypt(arg,password):
         print("Done decrypting.")
     else:
         print("Wrong password.")
-        os.remove(arg[:-8])
+        #os.remove(arg[:-8])
 
 def command(arg):
     if arg=="encrypt":
